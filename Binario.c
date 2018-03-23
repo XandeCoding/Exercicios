7f82b7d4ce2c37dc579a2c76f7b9837bea5f6402
Alexandre Fernandes dos Santos <af.santos1998@gmail.com>
2018-03-23 15:05:28 -0300

    Update Binario.c

--- Binario.c
+++ Binario.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int binario(int num){
-    int numero = num%2;
+
     if (num == 0){
         return (0);
     }
     binario(num/2);
-    return (printf("%d", numero));
+    return (printf("%d", num%2));
 }
 
 int main(){
